e7a782a9ab0b9318208400dd2eaa393ba9b3c105
Douglas Domingos <doug.domingossilva@gmail.com>
2022-07-06 15:23:42 -0300

    feat: add note repository interfaces

---
+++ src/repository/NotesRepository.ts
@@ -0,0 +1,12 @@
+export interface NotesRequestData {
+  noteID?: number
+  title: string;
+  content: string;
+}
+
+export interface NoteRepository {
+  getNotes: () => Promise<void>;
+  createNote: (data: NotesRequestData) => Promise<void>;
+  editNote: (data: NotesRequestData) => Promise<void>;
+  deleteNote: (noteID: number) => Promise<void>;
+}
