133f0b073f994f6fff99c01083b63896a4c2e406
Douglas Domingos <doug.domingossilva@gmail.com>
2022-07-06 14:27:27 -0300

    refactor: add param and return types to routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,19 +1,20 @@
 import express from "express";
+import { Request, Response } from "express";
 
 export const routes = express.Router();
 
-routes.get("/notes", (req, res) => {
+routes.get("/notes", (req: Request, res: Response): void => {
   res.send("See all notes");
 });
 
-routes.post("/notes/new", (req, res) => {
+routes.post("/notes/new", (req: Request, res: Response): void => {
   res.send("Create a new note");
 });
 
-routes.put("/notes/edit", (req, res) => {
+routes.put("/notes/edit", (req: Request, res: Response): void => {
   res.send("Edit a note");
 });
 
-routes.delete("/notes/delete", (req, res) => {
-  res.send("Delete a note")
-})
+routes.delete("/notes/delete", (req: Request, res: Response): void => {
+  res.send("Delete a note");
+});
