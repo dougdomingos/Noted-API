e65430d592c1788aedcc5907025421764a0fd448
Douglas Domingos <doug.domingossilva@gmail.com>
2022-07-07 07:39:30 -0300

    feat: add create note service

---
+++ src/services/CreateNoteService.ts
@@ -0,0 +1,22 @@
+import { NotesRepository, NotesRequestData } from "../models/NotesRepository";
+
+export class CreateNoteService {
+  constructor(private notesRepository: NotesRepository) {}
+
+  async execute(request: NotesRequestData) {
+    const { title, content } = request;
+
+    if (!title) {
+      throw new Error("No title was provided!");
+    }
+
+    if (!content) {
+      throw new Error("Content is required to create a note!");
+    }
+
+    await this.notesRepository.createNote({
+      title,
+      content,
+    });
+  }
+}
