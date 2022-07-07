0c070b79e3b1163c00cc72b1aa00ba01a3cdfbf4
Douglas Domingos <doug.domingossilva@gmail.com>
2022-07-07 10:54:43 -0300

    feat: add edit note service

---
+++ src/services/EditNoteService.ts
@@ -0,0 +1,23 @@
+import { NotesRepository, NotesRequestData } from "../models/NotesRepository";
+
+export class EditNoteService {
+  constructor(private notesRepository: NotesRepository) {}
+
+  async execute(request: NotesRequestData) {
+    const { noteID, title, content } = request;
+
+    if (!noteID) {
+      throw new Error("Note ID must be provided!")
+    }
+
+    if (!title && !content) {
+      throw new Error("Nothing to edit, since no values were provided.")
+    }
+
+    await this.notesRepository.editNote!({
+      noteID,
+      title,
+      content,
+    });
+  }
+}
