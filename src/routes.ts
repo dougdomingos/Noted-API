42dd52ff9c889bee6b6ff9abdfd9b84e7c75da88
Douglas Domingos <doug.domingossilva@gmail.com>
2022-07-09 10:08:27 -0300

    feat: implement create note feature

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import { Request, Response } from "express";
 
+import { PrismaNotesRepository } from "./models/prisma/PrismaNotesRepository";
+
+import { CreateNoteService } from "./services/CreateNoteService";
+
 export const routes = express.Router();
 
 routes.get("/notes", (req: Request, res: Response): void => {
@@ -8,7 +12,21 @@ routes.get("/notes", (req: Request, res: Response): void => {
 });
 
 routes.post("/notes/new", (req: Request, res: Response): void => {
-  res.send("Create a new note");
+  const notesRepository = new PrismaNotesRepository();
+  const createNoteService = new CreateNoteService(notesRepository);
+
+  const { title, content } = req.body;
+
+  try {
+    createNoteService.execute({
+      title,
+      content,
+    });
+  } catch (error) {
+    res.status(400).json({ msg: error });
+  }
+
+  res.status(201).json({msg: "Operation sucessful!"});
 });
 
 routes.put("/notes/edit", (req: Request, res: Response): void => {
