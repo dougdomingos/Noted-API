02f90f9b35b697c220c702b679e0704de8a28348
Douglas Domingos <doug.domingossilva@gmail.com>
2022-07-09 14:32:11 -0300

    feat: add edit note feature

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import { database } from "./models/database";
 
 import { CreateNoteService } from "./services/CreateNoteService";
 import { DeleteNoteService } from "./services/DeleteNoteService";
+import { EditNoteService } from "./services/EditNoteService";
 import { GetNotesService } from "./services/GetNotesService";
 
 export const routes = express.Router();
@@ -39,8 +40,24 @@ routes.post("/notes/new", (req: Request, res: Response): void => {
   res.status(201).json({ msg: "Operation sucessful!" });
 });
 
-routes.put("/notes/edit", (req: Request, res: Response): void => {
-  res.send("Edit a note");
+routes.put("/notes/edit/:id", (req: Request, res: Response): void => {
+  const editNoteService = new EditNoteService(database);
+
+  const id = req.params.id;
+  const { title, content } = req.body;
+
+  try {
+    editNoteService.execute({
+      // @ts-ignore
+      id: id,
+      title,
+      content,
+    });
+  } catch (error) {
+    res.status(400).json({ msg: error });
+  }
+
+  res.status(200).json({ msg: "Note sucessfully updated!" });
 });
 
 routes.delete("/notes/delete/:id", (req: Request, res: Response): void => {
